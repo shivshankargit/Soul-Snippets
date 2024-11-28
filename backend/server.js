03e9c405f79aab1daa7bbb65d1acdc011a93f4db
Shivshankargit <sa866096@gmail.com>
2024-11-28 22:58:03 +0530

    Fix CORS issue and update server configuration

--- backend/server.js
+++ backend/server.js
@@ -10,9 +10,8 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-//const __dirname = path.resolve();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true}));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true}));
 app.use(express.json());
 app.use(cookieParser());
 
