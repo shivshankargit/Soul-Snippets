64b0729892d52b796addb6a6951f911507a041ba
Shivshankargit <sa866096@gmail.com>
2024-11-30 16:17:41 +0530

    modified code in authStore.js

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -17,7 +17,7 @@ export const useAuthStore = create((set) => ({
     signup: async(email, password, username) => {
         set({isLoading: true,  error: null});
         try{
-            const response = await axios.post(`${API_URL}/signup`,{email,password,username});
+            const response = await axios.post('/signup',{email,password,username});
             set({user:response.data.user, isAuthenticated:true, isLoading: false});
         } catch(error) {
             set({error:error.response.data.message || "Error signing up", isLoading: false});
